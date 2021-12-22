9bdc1d72896ad9091a83a8064a52e6ccbe66c100
robinlashae1 <robinlashae1@gmail.com>
2021-12-21 23:22:14 -0500

    added icons and image

--- client/src/HomePage.js
+++ client/src/HomePage.js
@@ -7,12 +7,13 @@ import NavBar from './NavBar';
         <div className="homePage fullpage">
             <NavBar/>
             <div id="contact">
-                <a href="https://github.com/robinlashae1"className="linkDiv"><i className="bi-github" role="img" aria-label="Check out my GitHub"></i></a>
-                <a href="https://github.com/robinlashae1"className="linkDiv"><i className="bi-google" role="img" aria-label="Email Me"></i></a>
-                <a href="www.linkedin.com/in/robin-vernon"className="linkDiv"><i className="bi-linkedin" role="img" aria-label="Lets connect on Linkedin"></i></a>
-                <a href="https://github.com/robinlashae1"className="linkDiv"><i className="bi-slack" role="img" aria-label="Find me on Slack"></i></a>
+                <a href="https://github.com/robinlashae1"className="linkDiv"><i className="bi-github" role="img" aria-label="Check out my GitHub" style={activeStyles}></i></a>
+                <a href="https://github.com/robinlashae1"className="linkDiv"><i className="bi-google" role="img" aria-label="Email Me" style={activeStyles}></i></a>
+                <a href="https://linkedin.com/in/robin-vernon"className="linkDiv"><i className="bi-linkedin" role="img" aria-label="Lets connect on Linkedin" style={activeStyles}></i></a>
+                <a href="https://github.com/robinlashae1"className="linkDiv"><i className="bi-slack" role="img" aria-label="Find me on Slack" style={activeStyles}></i></a>
             </div>
-            <div id="portfolioImage"/>
+            <img src='../portfolioImage.png' id="portfolioImage"/>
+            <portfolioImage/>
             <a href="https://github.com/robinlashae1"className="linkDiv"><i className="bi-info-circle-fill" role="img" aria-label="More about me"></i></a>
             <h3 className='intro'>Hi, I'm</h3>
             <h1 className='intro'>Robin Vernon</h1>
@@ -22,3 +23,8 @@ import NavBar from './NavBar';
 
 };
 export default HomePage;
+const activeStyles={
+    fontSize: "40px",
+    marginLeft:"5px",
+    marginTop: "5%"
+}
