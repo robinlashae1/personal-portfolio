c4bffee064c9ad91b6b28e5abe5503d0e5fa11e0
robinlashae1 <robinlashae1@gmail.com>
2022-03-21 03:05:49 -0400

    fixed about

--- client/src/AboutMe.js
+++ client/src/AboutMe.js
@@ -8,7 +8,7 @@ import NavBar from './NavBar';
             <h1 className='title'>{title}</h1>
             <NavBar />
             <div className='textSpace'>
-            <p className='addLip'>My name is Robin Vernon and I am a full-stack  Developer, from/currently living in Philadelphia, PA. I have a Certificate in Software Development from the Flatiron School, and my primary interest is in Front in development. In my free time, I do puzzles, organize my life and work of side projects. I am a fast learner, who loves to try new things. I particularly enjoy web Development because of how closely coding resembles a puzzle. </p>
+            <p >My name is Robin Vernon and I am a full-stack  Developer, from/currently living in Philadelphia, PA. I have a Certificate in Software Development from the Flatiron School, and my primary interest is in Front in development. In my free time, I do puzzles, organize my life and work of side projects. I am a fast learner, who loves to try new things. I particularly enjoy web Development because of how closely coding resembles a puzzle. </p>
             <h3 className='aboutTitle'>Languages List</h3>
             <p>I have been studying web development for almost a year. At the flatiron school, I learned many things. I have worked on projects in both front and back-end development, and Ibe worked in HTML/HTML5, CSS/CSS3, JavaScript, React, Ruby, RubyonRails,jQuery, and SQL/MySQL/PostgreSQL, to name a few. I have worked on both group projects, and solo projects. </p>
             <h3 className='aboutTitle'>This is a Rails app</h3>
